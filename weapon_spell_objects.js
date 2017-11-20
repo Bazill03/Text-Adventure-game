a4c31253bf461727d62a3f682e9d9d986e9321a5
Bazill03 <boofoundyou03@gmail.com>
2017-11-20 15:02:55 -0800

    Added 3 armors and 1 weapon.

--- weapon_spell_objects.js
+++ weapon_spell_objects.js
@@ -21,6 +21,13 @@ var steelMace = {
   sound: maceAttackSound
 };
 
+var clayMore = {
+  name: "Claymore",
+  stats: 30,
+  damageType: "slash",
+  sound: swordAttackSound //to be replaced by cooler sound
+};
+
 //spells
 
 var fireBall = {
@@ -36,4 +43,20 @@ var healthPot = {
   stats: 35,
   owned: 2,
   sound: healthPotSound
+};
+
+//armor
+let rags = {
+  stats: 0,
+  description: "You look over your 'rags' despairingly. You feel cold, and vulnerable."
+};
+
+let cultistRobe{
+  stats: 5,
+  description: "The robes of the followers of Thaddius. Cultists will believe you are one of their brothers."
+}
+
+let chainMail = {
+  stats: 10,
+  description: "You look over your 'chainmail' with pride." //To be replaced by cooler text with context.
 };
